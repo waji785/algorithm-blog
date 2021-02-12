4b3f7b1bb59a93d92c6e68e8aab14a04e1040454
unknown <2462177270@qq.com>
2021-02-12 16:20:06 +0800

    用jar包隐藏方法细节

---
+++ EvlJar/src/com/aa/SalaryCaler.java
@@ -0,0 +1,11 @@
+package com.aa;
+
+public class SalaryCaler {
+    public SalaryCaler() {
+    }
+    public Double cal(Double salary) {
+
+        return salary*2;
+    }
+
+}
---
+++ classload/src/DemoOA.java
@@ -0,0 +1,21 @@
+import java.net.URL;
+import java.net.URLClassLoader;
+//将方法写入jar包并读取调用，目的是混淆隐藏实际调用的方法细节
+public class DemoOA {
+    public static void main(String[] args) throws Exception {
+        Double salary = 2000.00;
+        Double money;
+        URL jarPath = new URL("file:F:\\algorithm-blog\\EvlJar\\ide-eval-resetter-2.1.13.jar");
+        URLClassLoader urlClassLoader = new URLClassLoader(new URL[] {jarPath});
+        do {
+            money = calSalary(salary,urlClassLoader);
+            System.out.println("实际到手工资" + money);
+            Thread.sleep(1000);
+        } while (true);
+        }
+        private static Double calSalary(Double salary,ClassLoader ClassLoader) throws Exception{
+            Class<?> clazz = ClassLoader.loadClass("com.aa.SalaryCaler");
+            Object object = clazz.newInstance();
+            return (Double)clazz.getMethod("cal",Double.class).invoke(object,salary);
+    }
+}
